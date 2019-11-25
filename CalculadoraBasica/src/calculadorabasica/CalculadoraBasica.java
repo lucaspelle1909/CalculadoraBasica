e3129f476694f0adaf5cc425e8fff21d120b5aaa
Lucas <lucas.pelle@hotmail.com>
2019-11-25 20:07:24 -0300

    Criação do método somar

--- CalculadoraBasica/src/calculadorabasica/CalculadoraBasica.java
+++ CalculadoraBasica/src/calculadorabasica/CalculadoraBasica.java
@@ -14,6 +14,11 @@ public class CalculadoraBasica {
     /**
      * @param args the command line arguments
      */
+    
+    public static int somar(int n1, int n2){
+        return n1 + n2;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
