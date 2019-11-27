43e48971e15a8b7932816a40502e919bb7d3dac0
05220173 <05220173@d54964.farroupilha.ifrs.edu.br>
2019-11-26 22:49:35 -0200

    Classe conta

---
+++ CalculadoraBasica/src/calculadorabasica/Conta.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorabasica;
+
+/**
+ *
+ * @author 05220173
+ */
+public class Conta {
+
+    public static double somar(double n1, double n2) {
+        return n1 + n2;
+    }
+
+    public static double diminuir(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public static double dividir(double n1, double n2) {
+        return n1 / n2;
+    }
+
+    public static double multiplicar(double n1, double n2) {
+        return n1 * n2;
+    }
+}
